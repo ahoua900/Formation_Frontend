8240b80b7f665ebb8c34272df8ea6ae7a5a04f81
ahoua elvis ghislain <elvisahoua@gmail.com>
2020-12-03 17:26:15 +0000

    commit

--- Landrick/lan.js
+++ Landrick/lan.js
@@ -4,8 +4,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
     document.getElementById("img-dark").style.display = "flex";
+
     document.getElementById("img-light").style.display = "none";
     document.getElementById("mynav").style.backgroundColor = "White";
+    document.getElementById("mynav").style.transition = ".6s";
     document.getElementById("menu").classList.remove("menu");
     document.getElementById("menu").classList.add("menus");
     document.getElementById("buy").style.backgroundColor = "rgba(50, 194, 236)";
@@ -15,6 +17,7 @@ function scrollFunction() {
     document.getElementById("img-light").style.display = "flex";
     document.getElementById("img-dark").style.display = "none"
     document.getElementById("mynav").style.backgroundColor = "transparent";
+    document.getElementById("mynav").style.transition = ".6s";
     document.getElementById("menu").classList.remove("menus");
     document.getElementById("menu").classList.add("menu");
     document.getElementById("buy").style.backgroundColor = "White";
